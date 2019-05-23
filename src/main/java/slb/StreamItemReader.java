6993c0acf0a0c5afc56ccb8589b1e93b1122ca27
Zi Li <820972562@qq.com>
2019-05-23 13:11:16 +0800

    review previous code to test

--- src/main/java/slb/StreamItemReader.java
+++ src/main/java/slb/StreamItemReader.java
@@ -2,37 +2,41 @@ package slb;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Reads a stream of StreamItems from a file.
  */
 public class StreamItemReader {
-	private BufferedReader in;
+    private BufferedReader in;
 
-	public StreamItemReader(BufferedReader input) {
-		this.in = input;
-	}
+    public StreamItemReader(BufferedReader input) {
+        this.in = input;
+    }
 
-	public StreamItem nextItem() throws IOException {
-		String line = null;
-		try {
-			line = in.readLine();
-		} catch (IOException e) {
-			System.err.println("Unable to read from file");
-			throw e;
-		}
+    public StreamItem nextItem() throws IOException {
+        String line = null;
+        try {
+            line = in.readLine();
+        } catch (IOException e) {
+            System.err.println("Unable to read from file");
+            throw e;
+        }
 
-		if (line == null || line.length() == 0)
-			return null;
+        if (line == null || line.length() == 0)
+            return null;
 
-//		String[] tokens = line.split("\t");
-		String[] tokens = line.split("\\s+");
-		if (tokens.length < 2)
-			return null;
 
-		long timestamp = Long.parseLong(tokens[0]);
-//		String[] words = tokens[1].split(" ");
-		return new StreamItem(timestamp, Arrays.asList(tokens[1], tokens[2], tokens[3]));
-	}
+        long timestamp = Long.parseLong(line.split(" ")[0]);
+        String[] words = line.split(" ");
+
+        List<String> lists = new ArrayList<>();
+        for (int i = 1; i < words.length; i++) {
+            lists.add(words[i]);
+        }
+
+        return new StreamItem(timestamp, lists);
+    }
 }
