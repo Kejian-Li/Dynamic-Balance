09027e93ad6ff61db7ac54deb040e9f7cbea27a8
Zi Li <820972562@qq.com>
2019-05-25 12:46:35 +0800

    [Stream Load Balance] change core score formula

--- src/main/java/slb/LBDB.java
+++ src/main/java/slb/LBDB.java
@@ -1,5 +1,6 @@
 package slb;
 
+import com.clearspring.analytics.stream.cardinality.CardinalityMergeException;
 import com.google.common.hash.Hashing;
 import util.cardinality.HyperLogLog;
 import util.load.CountEntry;
@@ -15,7 +16,9 @@ public class LBDB implements LoadBalancer {
     private List<Server> nodes;
     private int numSources;
     private int serverNum;
-    private long[][] localLoad;  // local load for sources
+
+    private long[] totalLoad; // record total load through every upstream source
+    private long[][] localLoad;  // local load for downstream servers
 
     private long loadSamplingGranularity;
 
@@ -24,8 +27,9 @@ public class LBDB implements LoadBalancer {
     private float alpha;  // load and cardinality balance factor, default = 0.5
 
     private List<LossyCounting> lossyCountings;
-    private ArrayList<HyperLogLog[]> lists;
-    private final static double DEFAULT_STANDARD_DEVIATION = 0.4;  
+    private HyperLogLog[] nodesCardinality; // for upstream nodes
+    private ArrayList<HyperLogLog[]> lists;  // for downstream servers
+    private final static double DEFAULT_STANDARD_DEVIATION = 0.4;
 
     public LBDB(List<Server> nodes, int numSources, float delta, float alpha) {
 
@@ -36,6 +40,7 @@ public class LBDB implements LoadBalancer {
         this.error = delta * 0.1;
         this.alpha = alpha;
 
+        totalLoad = new long[numSources];
         localLoad = new long[numSources][serverNum];
 
         this.loadSamplingGranularity = nodes.get(0).getGranularity();
@@ -49,6 +54,8 @@ public class LBDB implements LoadBalancer {
 
         for (int i = 0; i < numSources; i++) {
             lossyCountings.add(new LossyCounting<>(error));
+            nodesCardinality[i] = new HyperLogLog(DEFAULT_STANDARD_DEVIATION);
+
             HyperLogLog[] hyperLogLogs = new HyperLogLog[serverNum];
             for (int j = 0; j < serverNum; j++) {
                 hyperLogLogs[j] = new HyperLogLog(DEFAULT_STANDARD_DEVIATION);
@@ -62,11 +69,13 @@ public class LBDB implements LoadBalancer {
     private HyperLogLog[] hyperLogLogs;
     private double[] scores = new double[serverNum];  // reuse for each incoming element
 
-
     @Override
     public Server getSever(long timestamp, Object key) {
         lossyCounting = lossyCountings.get(source);
         hyperLogLogs = lists.get(source);
+
+        totalLoad[source]++;  // record total load
+        nodesCardinality[source].offer(key); // record cardinality through this source
 
         int selected; // index of chosen server
         List<CountEntry<Object>> frequentItems = null;
@@ -81,13 +90,13 @@ public class LBDB implements LoadBalancer {
             selected = hash(key);
         } else {
             for (int i = 0; i < serverNum; i++) {
-                scores[i] = getScore(i);
+                scores[i] = getScore(i, key);
             }
             selected = findIndexOfMaxScore();
         }
 
         localLoad[source][selected]++;  // update local load for selected node
-        hyperLogLogs[selected].offer(key);  // update cardinality for selected node
+        hyperLogLogs[selected].offer(key);  // update cardinality for selected node and return false
 
         source++;  // next source to emit the key in round-robin fashion
         if (source == numSources) {
@@ -96,8 +105,24 @@ public class LBDB implements LoadBalancer {
         return nodes.get(selected);
     }
 
-    private double getScore(int i) {
-        return alpha * hyperLogLogs[i].cardinality() + (1 - alpha) * localLoad[source][i];
+    private int indicator;
+    private HyperLogLog tempHyperLogLog;
+
+    private double getScore(int i, Object key) {
+        try {
+            tempHyperLogLog = new HyperLogLog(DEFAULT_STANDARD_DEVIATION);
+            tempHyperLogLog.addAll(hyperLogLogs[i]);
+        } catch (CardinalityMergeException e) {
+            System.out.println(e);
+        }
+        if (tempHyperLogLog.offer(key)) {  // affect cardinality( do not contain this key before): there is precision
+            indicator = 0;
+        } else {  // unaffect cardinality
+            indicator = 1;
+        }
+        tempHyperLogLog = null; // for GC
+        return indicator + alpha * (1 - hyperLogLogs[i].cardinality() / nodesCardinality[source].cardinality())
+                + (1 - alpha) * (1 - localLoad[source][i] / totalLoad[source]);
     }
 
     private int findIndexOfMaxScore() {
