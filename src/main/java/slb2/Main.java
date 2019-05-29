6ab22e3a6b89f5023da8a92c4f08bfeb566bcfad
Zi Li <820972562@qq.com>
2019-05-29 14:18:59 +0800

    [Stream Load Balance] add output argument

--- src/main/java/slb2/Main.java
+++ src/main/java/slb2/Main.java
@@ -10,56 +10,46 @@ public class Main {
 
         final int simulatorType = Integer.parseInt(args[0]);
         final String inFilePathName = args[1];
-        final int numSources = Integer.parseInt(args[2]);  // number of upstream operators
-        final int numServers = Integer.parseInt(args[3]);  // number of downstream operators
+        final String outFilePathName = args[2];
+        final int numSources = Integer.parseInt(args[3]);  // number of upstream operators
+        final int numServers = Integer.parseInt(args[4]);  // number of downstream operators
 
         // default
         int threshold = 5;   // frequency threshold of Head
         float epsilon = 0.0001f;   // lossy count frequency threshold
 
         if (simulatorType == 3 || simulatorType == 4 || simulatorType == 5) {
-            threshold = Integer.parseInt(args[4]);
+            threshold = Integer.parseInt(args[5]);
         }
 
         if (simulatorType == 3) {
-            epsilon = Float.parseFloat(args[5]);
+            epsilon = Float.parseFloat(args[6]);
         }
 
         float delta = 0.2f;
         float alpha = 0.4f;
         if (simulatorType == 7) {
-            delta = Float.parseFloat(args[4]);
-            alpha = Float.parseFloat(args[5]);
+            delta = Float.parseFloat(args[5]);
+            alpha = Float.parseFloat(args[6]);
         }
 
         StreamPartitioner partitioner = null;
-        String outputName = null;
 
         if (simulatorType == 1) {
             partitioner = new HashPartitioner(numServers);
-            outputName = "hash";
         } else if (simulatorType == 2) {
             partitioner = new PKG_Partitioner(numServers);
-            outputName = "pkg";
         } else if (simulatorType == 3) {
             partitioner = new DChoices_Partitioner(numServers, threshold, epsilon);
-            outputName = "d-choices";
         } else if (simulatorType == 4) {
             partitioner = new WChoices_Partitioner(numServers, threshold);
-            outputName = "w-choices";
         } else if (simulatorType == 5) {
             partitioner = new RR_Partitioner(numServers, threshold);
-            outputName = "RR";
         } else if (simulatorType == 6) {
             partitioner = new SG_Partitioner(numServers);
-            outputName = "shuffle";
         } else if (simulatorType == 7) {
             partitioner = new HolisticPartitioner(numServers, delta, alpha);  //epsilon -> alpha
-            outputName = "holistic";
         }
-
-        String outFilePathName = "C:\\Users\\lizi\\Desktop\\分布式流处理系统的数据分区算法研究\\paper_writing\\twitter_"
-                + outputName + ".csv";
 
         Simulator simulator = new Simulator(numSources, numServers, inFilePathName, outFilePathName, partitioner);
         simulator.start();
